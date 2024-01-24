695639cc077ad10b4cec93ca57967b71864dd038
John Snow <qaz20042004@gmail.com>
2024-01-24 14:25:54 +0300

    App starting interface fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,7 +11,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        hello();
+//        hello();
         Engine.startGame(sendChosenOpt());
     }
     public static void hello() {
