173193ccc36b422a38fff66ee389083414c1bbdd
John Snow <qaz20042004@gmail.com>
2024-02-29 14:15:30 +0300

    remove get method from Cli. Added System.in closing.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,22 @@
 package hexlet.code;
 
 import java.util.Scanner;
-
 public class App {
+    private static String playerName;
     public static void main(String[] args) {
         Engine.setChosenGame(getGameChose());
         if (Engine.getChosenGame().equals("1")) {
-            Cli.hello();
+            playerName = Cli.hello();
         } else if (!Engine.getChosenGame().equals("0")) {
-            Cli.hello();
+            playerName = Cli.hello();
             Engine.rounds();
         }
+        Scanner closingSystemIn = new Scanner(System.in);
+        closingSystemIn.close();
+    }
+
+    public static String getPlayerName() {
+        return playerName;
     }
     public static String getGameChose() {
         System.out.println("Please enter the game number and press Enter.");
@@ -23,7 +29,6 @@ public class App {
         System.out.println("0 - Exit");
         Scanner gameChoseInput = new Scanner(System.in);
         var answer = gameChoseInput.next();
-        //gameChoseInput.close();
         return answer;
     }
 }
